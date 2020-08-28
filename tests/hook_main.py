8f0b6d2d563e3e3270a7da7b0e6b17fa9ca80449
blossom2017 <maitrishastri2016@gmail.com>
2020-08-28 19:09:50 +0530

    your token

--- tests/hook_main.py
+++ tests/hook_main.py
@@ -4377,7 +4377,7 @@ def verify_email_from_token(transaction):
         db.session.add(user)
         db.session.commit()
         request_body = json.loads(transaction['request']['body'])
-        request_body['data']['token'] = obtain_token("email@example.com", "password")
+        request_body['data']['token'] = "your token"
         # transaction['request']['headers']['Authorization'] = ""
         transaction['request']['body'] = json.dumps(request_body)
 
